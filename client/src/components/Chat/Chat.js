0a605e2446231b83cbf545ba98401db11cf24401
alanmaranto <alanarostegui@hotmail.com>
2019-12-09 20:37:55 -0600

    client,server: connecting chat with server

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,15 @@ const Chat = ({ location }) => {
         setName(name);
         setRoom(room);
 
-        socket.emit('join', { name: name, room: room });
+        socket.emit('join', { name: name, room: room }, () => {
+
+        });
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
 
     }, [ENDPOINT, location.search])
     return ( 
